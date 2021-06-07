08a16eef1de8955009f58e73025f4514b7e494c7
luiz123o <luiz123o@hotmail.com>
2021-06-07 10:35:57 -0300

    :lipstick: Adicionado thema e global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,23 +1,57 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+
+
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 300;
+  font-display: swap;
+  src: local(''),
+       url('../fonts/roboto-v27-latin-300.woff2') format('woff2'),
+
+}
+
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: local(''),
+       url('../fonts/roboto-v27-latin-regular.woff2') format('woff2'),
+
+}
+
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-display: swap;
+  src: local(''),
+       url('../fonts/roboto-v27-latin-700.woff2') format('woff2'),
+
+}
+${({ theme }) => css`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html {
     font-size: 62.5%;
   }
 
-  html, body, #__next {
-    height: 100%;
-  }
-
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
   }
+`}
+
+
 `
 
 export default GlobalStyles
---
+++ src/styles/theme.ts
@@ -0,0 +1,55 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '0.4rem'
+  },
+  font: {
+    family:
+      "Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem'
+    },
+    weight: {
+      light: 300,
+      normal: 400,
+      bold: 700
+    }
+  },
+
+  colors: {
+    primary: '#35E0FC',
+    secondary: '#342EAF',
+    bg: '#FDFCFD',
+    white: '#fafafa',
+    black: '#030517',
+    lightPurple: '#9200FC',
+    darkPurple: '#22013A',
+    lightGray: ' #EAEAEA',
+    gray: '#8f8f8f',
+    darkGray: '#2e2f42'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  }
+}
